47779cbe1ead52194a8b153a5ab89814566f8c8b
Edgar Ortiz <ed.ortizm@gmail.com>
2021-07-10 18:07:22 -0400

    scripts working :)

--- horizontal.py
+++ horizontal.py
@@ -4,5 +4,6 @@ import sys
 import pyautogui as ag
 
 x = sys.argv[1]
+x = int(x)
 
 ag.move(x, 0, duration=1.)
--- vertical.py
+++ vertical.py
@@ -4,5 +4,6 @@ import sys
 import pyautogui as ag
 
 y = sys.argv[1]
+y = int(y)
 
 ag.move(0, y, duration=1.)
